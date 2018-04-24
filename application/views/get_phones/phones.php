d828424afa1349ab5aebe4a580029ffebb2b127f
cevyfauzan <cevyfauzan@gmail.com>
2018-04-24 12:17:04 +0700

    v2.0.0

--- application/views/get_phones/phones.php
+++ application/views/get_phones/phones.php
@@ -59,7 +59,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title">ADD NEW SCRIPT</h4>
+				<h4 class="modal-title">ADD NEW PHONES</h4>
 			</div>
 			<div class="modal-body">
                 <div class="row">
@@ -137,12 +137,12 @@
 </div>
 
 <!-- Modal Edit -->
-<div id="edit-phonesssss" class="modal fade" role="dialog">
+<div id="edit-phones" class="modal fade" role="dialog">
 	<div class="modal-dialog" style="width:70%;">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title">ADD NEW SCRIPT</h4>
+				<h4 class="modal-title">MODIFY PHONES</h4>
 			</div>
 			<div class="modal-body">
                 <div class="row">
