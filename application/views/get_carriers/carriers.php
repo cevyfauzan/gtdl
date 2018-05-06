45bbd6c9045f56ae76fa75a3e1ab34ee8cf66247
cevyfauzan <cevyfauzan@gmail.com>
2018-05-06 19:01:05 +0700

    carrier

--- application/views/get_carriers/carriers.php
+++ application/views/get_carriers/carriers.php
@@ -11,11 +11,11 @@
 			<div class="box-header">
                 <h2 class="box-title"><b>Carriers</b></h2>
 				<div class="pull-right">
-                    <a href="" class="btn btn-success btn-sm" data-toggle="modal" data-target="#add-script" title="Add"><i class="fa fa-plus"></i>&ensp;Add New Carrier</a>
+                    <a href="" class="btn btn-success btn-sm" data-toggle="modal" data-target="#add-carrier" title="Add"><i class="fa fa-plus"></i>&ensp;Add New Carrier</a>
 				</div>
             </div>
             <div class="box-body">
-            <table id="call_time" class="table table-bordered table-striped">
+            <table id="carrier" class="table table-bordered table-striped">
                 <thead>
                     <tr>
                         <th>Carrier ID</th>
@@ -37,7 +37,7 @@
                         <td style="color:green">Registered</td>
                         <td style="color:green">ACTIVE</td>
                         <td>
-                            <a href="" title="Edit" data-toggle="modal" data-target="#edit-script"><i class="fa fa-edit text-yellow"></i></a>&ensp;
+                            <a href="" title="Edit" data-toggle="modal" data-target="#edit-carrier"><i class="fa fa-edit text-yellow"></i></a>&ensp;
                             <a href="" title="Delete" onclick="return confirm('Are you sure you want to delete this data ?');"><i class="fa fa-remove text-red"></i></a>&ensp;
                             <a href="" title="Info"><i class="fa fa-info-circle text-info"></i></a>&ensp;
                         </td>
@@ -52,42 +52,118 @@
 
 <!--======================================================================================================================-->
 <!-- Modal Add -->
-<div id="add-script" class="modal fade" role="dialog">
+<div id="add-carrier" class="modal fade" role="dialog">
 	<div class="modal-dialog" style="width:70%;">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title">ADD NEW CALL TIMES</h4>
+				<h4 class="modal-title">ADD NEW CARRIER</h4>
 			</div>
 			<div class="modal-body">
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script ID :</label>
+							<label>Carrier ID :</label>
 						</div>
 					</div>
 					<div class="col-sm-3">
-						<input type="text" class="form-control" name="" value="script001" readonly>
+						<input type="text" class="form-control" name="">
 					</div>
 				</div>
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script Name :</label>
+							<label>Carrier Name :</label>
 						</div>
 					</div>
-					<div class="col-sm-5">
-						<input type="text" class="form-control" name="" value="">
+					<div class="col-sm-3">
+						<input type="text" class="form-control" name="">
 					</div>
 				</div>
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script Comments :</label>
+							<label>Carrier Desc :</label>
 						</div>
 					</div>
 					<div class="col-sm-5">
 						<input type="text" class="form-control" name="" >
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Admin User Group :</label>
+						</div>
+					</div>
+					<div class="col-sm-2">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => 'All User Groups','N' => 'Administrators','M' => 'Agents');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Account Entry :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+                       		<textarea class="form-control" name="" rows="10"></textarea>
+					   	</div>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Protocol :</label>
+						</div>
+					</div>
+					<div class="col-sm-2">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => 'SIP','N' => 'IAX');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Globals String :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<input type="text" class="form-control" name="" >
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Dialplan Entry :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+                       		<textarea class="form-control" name="" rows="7"></textarea>
+					   	</div>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Server :</label>
+						</div>
+					</div>
+					<div class="col-sm-3">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => '192.168.1.1 - getdial server');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
 					</div>
 				</div>
                 <div class="row">
@@ -102,29 +178,6 @@
 							$drop_down = array('Y' => 'YES','N' => 'NO');
 						?>
 						<?= form_dropdown('', $drop_down, '', $attr) ?>
-					</div>
-				</div>
-                <div class="row">
-					<div class="col-sm-4" align="right">
-						<div class="form-group">
-							<label>Script Text :</label>
-						</div>
-					</div>
-					<div class="col-sm-4">
-                        <?php 
-							$attr = 'class="form-control" onChange="change(this);"';
-							$drop_down = array('P' => 'YES','Y' => 'NO');
-						?>
-						<?= form_dropdown('', $drop_down, '', $attr) ?>
-					</div>
-					<div class="col-sm-4">
-                        <a href="" class="btn btn-primary btn-sm" ><i class="fa fa-plus"></i>&ensp;Insert</a>
-					</div>
-				</div>
-                <div class="row">
-					<div class="col-sm-4"></div>
-					<div class="col-sm-7">
-                       <textarea class="form-control" name="" rows="10"></textarea>
 					</div>
 				</div>
 				<br>
@@ -135,42 +188,118 @@
 </div>
 
 <!-- Modal Edit -->
-<div id="edit-script" class="modal fade" role="dialog">
+<div id="edit-carrier" class="modal fade" role="dialog">
 	<div class="modal-dialog" style="width:70%;">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title">ADD NEW SCRIPT</h4>
+				<h4 class="modal-title">MODIFY CARRIER</h4>
 			</div>
 			<div class="modal-body">
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script ID :</label>
+							<label>Carrier ID :</label>
 						</div>
 					</div>
 					<div class="col-sm-3">
-						<input type="text" class="form-control" name="" value="script001" readonly>
+						<input type="text" class="form-control" name="">
 					</div>
 				</div>
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script Name :</label>
+							<label>Carrier Name :</label>
 						</div>
 					</div>
-					<div class="col-sm-5">
-						<input type="text" class="form-control" name="" value="">
+					<div class="col-sm-3">
+						<input type="text" class="form-control" name="">
 					</div>
 				</div>
                 <div class="row">
 					<div class="col-sm-4" align="right">
 						<div class="form-group">
-							<label>Script Comments :</label>
+							<label>Carrier Desc :</label>
 						</div>
 					</div>
 					<div class="col-sm-5">
 						<input type="text" class="form-control" name="" >
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Admin User Group :</label>
+						</div>
+					</div>
+					<div class="col-sm-2">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => 'All User Groups','N' => 'Administrators','M' => 'Agents');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Account Entry :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+                       		<textarea class="form-control" name="" rows="10"></textarea>
+					   	</div>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Protocol :</label>
+						</div>
+					</div>
+					<div class="col-sm-2">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => 'SIP','N' => 'IAX');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Globals String :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<input type="text" class="form-control" name="" >
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Dialplan Entry :</label>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+                       		<textarea class="form-control" name="" rows="7"></textarea>
+					   	</div>
+					</div>
+				</div>
+                <div class="row">
+					<div class="col-sm-4" align="right">
+						<div class="form-group">
+							<label>Server :</label>
+						</div>
+					</div>
+					<div class="col-sm-3">
+                        <?php 
+							$attr = 'class="form-control"';
+							$drop_down = array('Y' => '192.168.1.1 - getdial server');
+						?>
+						<?= form_dropdown('', $drop_down, '', $attr) ?>
 					</div>
 				</div>
                 <div class="row">
@@ -187,32 +316,8 @@
 						<?= form_dropdown('', $drop_down, '', $attr) ?>
 					</div>
 				</div>
-                <div class="row">
-					<div class="col-sm-4" align="right">
-						<div class="form-group">
-							<label>Script Text :</label>
-						</div>
-					</div>
-					<div class="col-sm-4">
-                        <?php 
-							$attr = 'class="form-control" onChange="change(this);"';
-							$drop_down = array('P' => 'YES','Y' => 'NO');
-						?>
-						<?= form_dropdown('', $drop_down, '', $attr) ?>
-					</div>
-					<div class="col-sm-4">
-                        <a href="" class="btn btn-primary btn-sm" ><i class="fa fa-plus"></i>&ensp;Insert</a>
-					</div>
-				</div>
-                <div class="row">
-					<div class="col-sm-4"></div>
-					<div class="col-sm-7">
-                       <textarea class="form-control" name="" rows="10"></textarea>
-                       <a href="" class="btn btn-info btn-sm"><i class="fa fa-eye"></i>&ensp;Preview</a>
-					</div>
-				</div>
 				<br>
-                <center><a href="" class="btn btn-success btn-md">SAVE SETTING</a></center>
+                <center><a href="" class="btn btn-success btn-md">SUBMIT</a></center>
 			</div>
 		</div>			
 	</div>
@@ -221,7 +326,7 @@
 <!--======================================================================================================================-->
 <script>
     $(function () {
-		$('#call_time').DataTable({
+		$('#carrier').DataTable({
 			"ordering": false,
 			"autoWidth": false
 		});
