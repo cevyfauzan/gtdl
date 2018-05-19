1ec6671ae11735685ae8974e9c08ca018824df7c
cevyfauzan <cevyfauzan@gmail.com>
2018-05-19 13:35:47 +0700

    title login

--- application/views/login_agent.php
+++ application/views/login_agent.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>getDIAL.tech Login</title>
+        <title>The Next Generations Contact Centers</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <link rel="icon" href="<?php echo base_url()?>assets/dist/img/getdial-icon.png" type="image/x-icon" />
         <link rel="stylesheet" href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css">
